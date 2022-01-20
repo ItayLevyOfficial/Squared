ab4819428fccf7ac1bb9ec0807ef342aef9e5b9a
Barry Kuts <barrykuts@Barrys-MacBook-Pro.local>
2022-01-20 12:29:03 +0200

    modal layout initialized

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,37 +1,70 @@
-import StakingPools from "./StakingPools"
-import LiquidityPools from "./LiquidityPools"
-import LiquidityPool from "./LiquidityPool"
-import Sidenav from "../Layouts/Sidenav"
-import Header from "../Layouts/Header"
-import Footer from "../Layouts/Footer"
-import { InformationCircleIcon } from "@heroicons/react/outline"
+import { useState } from 'react'
+import StakingPool from './StakingPool'
+import LiquidityPools from './LiquidityPools'
+import LiquidityPool from './LiquidityPool'
+import Sidenav from '../Layouts/Sidenav'
+import Header from '../Layouts/Header'
+import Footer from '../Layouts/Footer'
+import DisplayModal from './DisplayModal'
+import { InformationCircleIcon } from '@heroicons/react/outline'
+
+import { StakingPoolsObject } from './StakingPools'
 
 const Products = () => {
+  const [modalIsOpen, setIsOpen] = useState(false)
+  const [selectedPool, setSelectedPool] = useState('')
+
+  const openModal = (id) => {
+    setIsOpen(true)
+    setSelectedPool(id)
+  }
+
+  const closeModal = () => {
+    setIsOpen(false)
+  }
+
   return (
-    <div className="w-full h-screen flex justify-center">
-      <Sidenav />
-      <div className="flex flex-col justify-between ml-6 w-9/12">
-        <Header />
-        <StakingPools />
-        <LiquidityPools
-          column1="DEPOSITS"
-          column2="APR on Deposits"
-          column3="My Vote"
-          column4="APR on Vote"
-          children={
-            <LiquidityPool
-              column1="$140M"
-              column2="14%"
-              column3="NONE"
-              column4={
-                <InformationCircleIcon className="h-6 cursor-pointer text-gray-100" />
-              }
-            />
-          }
-        />
-        <Footer />
+    <>
+      <DisplayModal
+        modalIsOpen={modalIsOpen}
+        closeModal={closeModal}
+        selectedPool={selectedPool}
+        children={StakingPoolsObject.map(
+          (el) =>
+            el.id == selectedPool && (
+              <StakingPool el={el} key={el.id} selectedPool={selectedPool} />
+            )
+        )}
+      />
+      <div className="w-full h-screen flex justify-center">
+        <Sidenav />
+        <div className="flex flex-col justify-between ml-6 w-9/12">
+          <Header />
+          <div className="w-full mb-6 text-gray-100 h-56 border-transparent rounded-3xl flex items-center justify-between p-6 space-x-4 overflow-x-scroll scrollbar scrollbar-thumb-green-200 scrollbar-track-gray-700 scrollbar-thin">
+            {StakingPoolsObject.map((el) => (
+              <StakingPool el={el} key={el.id} openModal={openModal} />
+            ))}
+          </div>
+          <LiquidityPools
+            column1="DEPOSITS"
+            column2="APR on Deposits"
+            column3="My Vote"
+            column4="APR on Vote"
+            children={
+              <LiquidityPool
+                column1="$140M"
+                column2="14%"
+                column3="NONE"
+                column4={
+                  <InformationCircleIcon className="h-6 cursor-pointer text-gray-100" />
+                }
+              />
+            }
+          />
+          <Footer />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
--- src/Products/StakingPool.jsx
+++ src/Products/StakingPool.jsx
@@ -1,21 +1,25 @@
-const StakingPool = (props) => {
+const StakingPool = ({ el, openModal }) => {
+  const { title, amount, apr, swap, button } = el
   return (
     <div className="w-48 bg-[#2F8652] text-gray-100 h-40  border-transparent rounded-3xl flex flex-col items-center justify-between p-2 hover:-translate-y-1 transition ease-in-out duration-300 flex-shrink-0">
-      <div className={`${props.font} underline`}>{props.title}</div>
+      <div className={`text-md underline`}>{title}</div>
 
       <div className="flex justify-between items-center w-full">
         <div className="flex flex-col items-center justify-center">
-          <div className="text-gray-100">{props.amount}</div>
+          <div className="text-gray-100">{amount}</div>
           <div className="text-xs text-gray-300">TVL</div>
         </div>
         <div className="flex flex-col items-center justify-center">
-          <div className="text-gray-100 text-sm">{props.apr}</div>
-          <div className="text-xs text-gray-300">{props.swap}</div>
+          <div className="text-gray-100 text-sm">{apr}</div>
+          <div className="text-xs text-gray-300">{swap}</div>
         </div>
       </div>
 
-      <button className=" bg-[#16473aec] w-11/12 p-2 text-sm rounded-lg">
-        {props.button}
+      <button
+        onClick={() => openModal(title)}
+        className=" bg-[#16473a] w-11/12 p-2 text-sm rounded-lg"
+      >
+        {button}
       </button>
     </div>
   )
