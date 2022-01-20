758b928f9209f3c67a52dcd43944835d37203c30
Itay Levy <itaylevy134@gmail.com>
2022-01-20 20:33:42 +0200

    Now the spacer is no longer in use

--- src/launchEvent/launchEventScreen.jsx
+++ src/launchEvent/launchEventScreen.jsx
@@ -3,11 +3,9 @@ import {AddressButton} from "../Layouts/Header";
 export const LaunchEventScreen = () => {
     return <ScreenPaddedContainer>
         <div className="flex flex-col items-center w-full max-w-screen-xl">
-            <LaunchEventHeader/>
-            <Spacer height="20"/>
+            <LaunchEventHeader className="mb-20"/>
             <div className="w-[60ch] flex flex-col">
                 <h1 className="text-white text-3xl font-medium tracking-wide mb-1.5">Commit Assets</h1>
-                <h2 className="text-primary text-lg tracking-wide font-light mb-3">4 DAYS 23 HOURS 59 MINUTES REMAIN</h2>
                 <p className="text-white font-light">
                     Commit FTM or MIM to swap for BAKD.
                     Final conversion rate is set at start of last look.
@@ -18,14 +16,14 @@ export const LaunchEventScreen = () => {
     </ScreenPaddedContainer>
 }
 
-const LaunchEventHeader = () => {
-    return <div className="flex items-center w-full justify-between">
+const LaunchEventHeader = ({className = ''}) => {
+    return <div className={`flex items-center w-full justify-between ${className}`}>
         <div className="flex">
             <div className="w-0.5 h-50 bg-primary"/>
             <div className="w-5"/>
             <div className="flex flex-col space-y-2 font-bold tracking-widest">
-                <h1 className="text-white text-5xl font">PUFF</h1>
-                <h2 className="text-primary text-xl tracking-widest font-light">DEGENESIS EVENT</h2>
+                <h1 className="text-white text-5xl">PUFF</h1>
+                <h2 className="text-primary text-xl font-light">DEGENESIS EVENT</h2>
             </div>
         </div>
         <AddressButton/>
