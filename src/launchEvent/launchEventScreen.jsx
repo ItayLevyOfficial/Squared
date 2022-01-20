42830265564a65b4cf0e69c5fb629163ddf10d89
Itay Levy <itaylevy134@gmail.com>
2022-01-20 12:56:40 +0200

    Now the degenesis top left title looks great

--- src/launchEvent/launchEventScreen.jsx
+++ src/launchEvent/launchEventScreen.jsx
@@ -1,11 +1,14 @@
 export const LaunchEventScreen = () => <ScreenPaddedContainer>
     <div className="flex w-full max-w-screen-2xl">
+        <div className="w-0.5 h-50 bg-primary"/>
+        <div className="w-5"/>
         <div className="flex flex-col space-y-2">
             <h1 className="text-white text-5xl font-bold tracking-wide">PUFF</h1>
             <h2 className="text-primary text-xl tracking-wide font-light">DEGENESIS EVENT</h2>
         </div>
     </div>
 </ScreenPaddedContainer>
+
 
 export const ScreenPaddedContainer = (props) => <div className="w-screen h-screen bg-black flex flex-col items-center px-40 py-10">
     {props.children}
