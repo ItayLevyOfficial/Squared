08a6a530e2dabdff76b49a4268e6bfc9c9e00008
Itay Levy <itaylevy134@gmail.com>
2022-01-20 12:06:48 +0200

    Now the screen container is in a great size

--- src/launchEvent/launchEventScreen.jsx
+++ src/launchEvent/launchEventScreen.jsx
@@ -1,4 +1,4 @@
-export const LaunchEventScreen = () => <div className="w-screen h-screen bg-black flex flex-col items-center px-40">
+export const LaunchEventScreen = () => <div className="w-screen h-screen bg-black flex flex-col items-center px-40 py-10">
     <div className="flex flex-col w-full max-w-screen-2xl bg-red-500 h-1.5">
 
     </div>
