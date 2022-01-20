bbb5af3f5db71a215c0bb822acb4b001eadda9cd
Itay Levy <itaylevy134@gmail.com>
2022-01-20 12:08:35 +0200

    small refactor

--- src/launchEvent/launchEventScreen.jsx
+++ src/launchEvent/launchEventScreen.jsx
@@ -1,5 +1,9 @@
-export const LaunchEventScreen = () => <div className="w-screen h-screen bg-black flex flex-col items-center px-40 py-10">
+export const LaunchEventScreen = () => <ScreenPaddedContainer>
     <div className="flex flex-col w-full max-w-screen-2xl bg-red-500 h-1.5">
 
     </div>
+</ScreenPaddedContainer>
+
+export const ScreenPaddedContainer = ({children}) => <div className="w-screen h-screen bg-black flex flex-col items-center px-40 py-10">
+    {children()}
 </div>
