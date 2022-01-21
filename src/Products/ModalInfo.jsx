882389059932c7145e75608325e6fa6cb1b8d8c0
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-01-21 15:55:25 +0200

    modal layout initialized (#2)
    * modal layout initialized
    
    * add modal
    
    * fix modal
    
    * fix modal text
    
    * change body color
    
    * fix modal input spacing
    
    * fix comments for modal
    
    Co-authored-by: Barry Kuts <barrykuts@Barrys-MacBook-Pro.local>

---
+++ src/Products/ModalInfo.jsx
@@ -0,0 +1,58 @@
+import ModalLine from './ModalLine'
+
+const ModalInfo = ({ isOnWithdraw, selectedToken }) => {
+  return (
+    <div className="text-white text-sm mt-6">
+      {isOnWithdraw ? (
+        <>
+          <ModalLine
+            title="Deposited"
+            amount="0.00"
+            selectedToken={selectedToken}
+          />
+          <br />
+          <ModalLine
+            title="Requested Withdrawl"
+            amount="0.00"
+            selectedToken={selectedToken}
+          />
+          <ModalLine
+            title="Available for Withdrawl"
+            amount="0.00"
+            selectedToken={selectedToken}
+          />
+          <br /> <br />
+          <div className="w-full flex flex-col items-center text-xs">
+            <span>
+              Subsequent withdrawal requests will overwrite existing requests
+            </span>
+
+            <span>
+              Requested withdrawals will become available after 1 cycle
+            </span>
+          </div>
+        </>
+      ) : (
+        <>
+          <ModalLine
+            title="Deposited"
+            amount="0.00"
+            selectedToken={selectedToken}
+          />
+          <br /> <br />
+          <div className="w-full flex flex-col items-center text-xs">
+            <span>Withdrawals from Puff pools are subject to a cooldown</span>
+
+            <span>
+              Requested withdrawals become available at the beginning of next
+              Cycle
+            </span>
+            <span>( within 24h )</span>
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default ModalInfo
--- src/Products/StakingPools.jsx
+++ src/Products/StakingPools.jsx
@@ -1,53 +1,42 @@
-import Footer from "../Layouts/Footer"
-import Header from "../Layouts/Header"
-import Sidenav from "../Layouts/Sidenav"
-import StakingPool from "./StakingPool"
-
-const StakingPools = () => {
-  return (
-    <div className="w-full mb-6 text-gray-100 h-56 border-transparent rounded-3xl flex items-center justify-between p-6 space-x-4 overflow-x-scroll scrollbar scrollbar-thumb-green-200 scrollbar-track-gray-700 scrollbar-thin">
-      <StakingPool
-        title="MIM"
-        font="text-2xl"
-        apr="7%"
-        amount="$300M"
-        swap="BAKD/MIM"
-        button="Depost MIM"
-      />
-      <StakingPool
-        title="FTM"
-        font="text-2xl"
-        apr="7%"
-        amount="$100M"
-        swap="BAKD/FTM"
-        button="Deposit FTM"
-      />
-      <StakingPool
-        title="BAKD"
-        font="text-2xl"
-        apr="VOTE TO EARN"
-        amount="$500M"
-        swap="BAKD"
-        button="STAKE AND VOTE"
-      />
-      <StakingPool
-        title="BAKD SPOOKY LP"
-        font="text-lg"
-        apr="165%"
-        amount="$148M"
-        swap="BAKD/MIN"
-        button="Depost Spooky LP"
-      />
-      <StakingPool
-        title="BAKD SPIRIT LP"
-        font="text-lg"
-        apr="170%"
-        amount="$148M"
-        swap="BAKD/MIN"
-        button="Depost Spirit LP"
-      />
-    </div>
-  )
-}
-
-export default StakingPools
+export const StakingPoolsObject = [
+  {
+    id: 1,
+    title: 'MIM',
+    apr: '7%',
+    amount: '$300M',
+    swap: 'BAKD/MIM',
+    button: 'Deposit MIM',
+  },
+  {
+    id: 2,
+    title: 'FTM',
+    apr: '7%',
+    amount: '$300M',
+    swap: 'BAKD/FTM',
+    button: 'Deposit FTM',
+  },
+  {
+    id: 3,
+    title: 'BAKD',
+    apr: '7%',
+    amount: '$300M',
+    swap: 'BAKD',
+    button: 'STAKE AND VOTE',
+  },
+  {
+    id: 4,
+    title: 'BAKD SPOOKY LP',
+    apr: '7%',
+    amount: '$300M',
+    swap: 'BAKD/MIM',
+    button: 'Deposit Spooky LP',
+  },
+  {
+    id: 5,
+    title: 'BAKD SPIRIT LP',
+    apr: '7%',
+    amount: '$300M',
+    swap: 'BAKD/MIM',
+    button: 'Deposit Spirit LP',
+  },
+]
