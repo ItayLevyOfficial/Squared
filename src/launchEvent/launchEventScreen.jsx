df1fdf88c66cc7f2083d48c99f40c8e8165f7f8d
Itay Levy <itaylevy134@gmail.com>
2022-01-22 18:57:55 +0200

    removed the commit assets section

--- src/launchEvent/launchEventScreen.jsx
+++ src/launchEvent/launchEventScreen.jsx
@@ -4,13 +4,6 @@ export const LaunchEventScreen = () => {
     return <ScreenPaddedContainer>
         <div className="flex flex-col items-center w-full max-w-screen-xl">
             <LaunchEventHeader/>
-            <div className="w-[60ch] flex flex-col">
-                <h1 className="text-white text-3xl font-medium tracking-wide mb-1.5">Commit Assets</h1>
-                <p className="text-white font-light">
-                    Commit FTM or MIM to swap for BAKD.
-                    Final conversion rate is set at start of last look.
-                </p>
-            </div>
         </div>
     </ScreenPaddedContainer>
 }
