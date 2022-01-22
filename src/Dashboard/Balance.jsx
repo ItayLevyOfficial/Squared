5d31d1bb0a87f4e28c5b6466011511beb4e815ea
Barry Kuts <barrykuts@gmail.com>
2022-01-22 13:14:56 +0200

    declare info box outside return

--- src/Dashboard/Balance.jsx
+++ src/Dashboard/Balance.jsx
@@ -1,12 +1,12 @@
-const Balance = () => {
-  const InformationBox = (props) => {
-    return (
-      <div className="w-full bg-dark  h-40  text-white rounded-xl flex flex-col items-center justify-between p-2 ">
-        <div className="underline">{props.text}</div>
-      </div>
-    )
-  }
+const InformationBox = (props) => {
+  return (
+    <div className="w-full bg-dark  h-40  text-white rounded-xl flex flex-col items-center justify-between p-2 ">
+      <div className="underline">{props.text}</div>
+    </div>
+  )
+}
 
+const Balance = () => {
   return (
     <div className="w-full mb-6 h-56  rounded-xl flex items-center justify-between p-6 space-x-4">
       <InformationBox text="APR" />
