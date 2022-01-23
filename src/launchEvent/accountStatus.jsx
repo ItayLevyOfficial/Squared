6cce4b790e9e80e30209337510d567afa3818fc3
Itay Levy <itaylevy134@gmail.com>
2022-01-23 16:51:10 +0200

    Now the deposit bnb button looks nice

--- src/launchEvent/accountStatus.jsx
+++ src/launchEvent/accountStatus.jsx
@@ -2,6 +2,9 @@ export const AccountStatus = () => {
     return <div className="flex flex-col">
         <h1 className="text-4xl font-medium tracking-wide mb-5">Your Account Status</h1>
         <h2 className="font-number text-base mb-2">BNB committed: $57</h2>
-        <h2 className="font-number text-base mb-5">BUSD committed: $57</h2>
+        <h2 className="font-number text-base mb-5">BUSD committed: $93</h2>
+        <button className="w-80 bg-[#F3BA2F] py-3 font-medium text-lg rounded-xl">
+            Deposit / Withdraw BNB
+        </button>
     </div>
 }
