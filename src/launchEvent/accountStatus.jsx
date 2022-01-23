0c9c57efe19d2969aa81ba14324c7a4c8ec4078d
Itay Levy <itaylevy134@gmail.com>
2022-01-23 18:46:49 +0200

    Now the accountStatus.jsx component is perfect

--- src/launchEvent/accountStatus.jsx
+++ src/launchEvent/accountStatus.jsx
@@ -1,21 +1,23 @@
 import BNBIcon from './BNB.svg'
 import BUSDIcon from './busd.svg'
 
-const DepositButton = ({backgroundColor, tokenName, iconSrc, className = '', textColor = "white"}) => <button
+const DepositButton = ({backgroundColor, tokenName, iconSrc, className = '', textColor = "white", onClick}) => <button
     className={`w-80 bg-[${backgroundColor}] py-3 font-medium text-lg rounded-xl flex ${className}
-     text-${textColor} hover:bg-opacity-95`}>
+     text-${textColor} hover:bg-opacity-95`} onClick={onClick}>
     <img src={iconSrc} alt="" className="ml-8 mr-4"/>
     Deposit / Withdraw&nbsp;{tokenName}
 </button>
 
 
-export const AccountStatus = () => {
+export const AccountStatus = ({bnbCommitted = 0, busdCommitted = 0, handleBnbClick, handleBusdClick}) => {
     return <div className="flex flex-col">
         <h1 className="text-4xl font-medium tracking-wide mb-5">Your Account Status</h1>
-        <h2 className="font-number text-base mb-2">BNB committed: $57</h2>
-        <h2 className="font-number text-base mb-5">BUSD committed: $93</h2>
-        <DepositButton backgroundColor="#F3BA2F" iconSrc={BNBIcon} tokenName="BNB" className="mb-5"/>
-        <DepositButton backgroundColor="#fff" iconSrc={BUSDIcon} tokenName="BUSD" textColor="black"/>
+        <h2 className="font-number text-base mb-2">BNB committed: ${bnbCommitted}</h2>
+        <h2 className="font-number text-base mb-5">BUSD committed: ${busdCommitted}</h2>
+        <DepositButton backgroundColor="#F3BA2F" iconSrc={BNBIcon} tokenName="BNB" className="mb-5"
+                       onClick={handleBnbClick}/>
+        <DepositButton backgroundColor="#fff" iconSrc={BUSDIcon} tokenName="BUSD" textColor="black"
+                       onClick={handleBusdClick}/>
     </div>
 }
 
