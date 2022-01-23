86ca6a6bba1439b3943c9f568d9c686bd635dd6d
Itay Levy <itaylevy134@gmail.com>
2022-01-23 22:33:33 +0200

    Created a great generic status bar component

--- src/launchEvent/EventStatus.jsx
+++ src/launchEvent/EventStatus.jsx
@@ -1,5 +1,20 @@
 import {BodyHeaderText} from './accountStatus'
 
+const StatusBar = ({percent, text, backgroundColorClass, className}) => {
+    const filledHeightClass = `h-${Math.round((percent / 100) * 12)}/12`
+
+    return <div className={`flex flex-col h-36 ${className}`}>
+        <div className={`flex items-end ${filledHeightClass}`}>
+            <div className="bg-white h-full rounded-t-lg w-5"/>
+            <div className="bg-white h-[0.5px] w-24"/>
+        </div>
+        <div className="flex h-2/3">
+            <div className={`bg-dark ${backgroundColorClass} rounded-b-lg w-5`}/>
+            <span className="font-number font-normal py-2 px-3">{percent}% {text}</span>
+        </div>
+    </div>
+}
+
 export const EventStatus = ({totalCommitments = 207_341, puffPrice = 8}) => {
     return <div className="flex flex-col">
         <BodyHeaderText title='Event Status' firstRow={`Total commitments: $${totalCommitments}`}
