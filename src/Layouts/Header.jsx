6710364b716210ce18082aab7f2ff67db188900b
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-01-23 19:44:58 +0200

    add ethers

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,8 +1,20 @@
 import Fantom from '../Assets/Fantom.svg'
 import { useLocation } from 'react-router-dom'
+import { ethers } from 'ethers'
 
 const Header = () => {
   const { pathname } = useLocation()
+
+  function getBalance() {
+    const url =
+      'https://winter-blue-resonance.bsc.quiknode.pro/8f059c17bb29420d84ff5cf99bbaea3be1eb081b/'
+
+    const provider = new ethers.providers.JsonRpcProvider(url)
+
+    console.log(provider)
+  }
+
+  getBalance()
 
   return (
     <nav className="flex h-20 space-x-2 items-center justify-between w-full mt-2 mb-4">
@@ -14,9 +26,9 @@ const Header = () => {
           <img src={Fantom} className="h-8 w-8" />
           <div>Fantom</div>
         </div>
-        <div className="w-36 h-10 flex justify-center   text-gray-100 bg-[#D05C47]  cursor-pointer rounded-xl p-2">
+        <button className="w-36 h-10 flex justify-center   text-gray-100 bg-[#D05C47]  cursor-pointer rounded-xl p-2">
           Connect Wallet
-        </div>
+        </button>
       </div>
     </nav>
   )
