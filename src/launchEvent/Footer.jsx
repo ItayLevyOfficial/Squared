8edcfb2a241dd913f321d23422b8f88794be6ef8
Itay Levy <itaylevy134@gmail.com>
2022-01-24 11:23:04 +0200

    Now the twitter.svg icon looks great on hover

--- src/launchEvent/Footer.jsx
+++ src/launchEvent/Footer.jsx
@@ -4,8 +4,14 @@ import DocsIcon from './footerIcons/docs.svg'
 import DiscordIcon from './footerIcons/discord.svg'
 import GithubIcon from './footerIcons/github.svg'
 
+const FooterIcon = ({src}) => <img src={src} alt="" className="opacity-70 hover:opacity-100"/>
+
 export const Footer = () => <div className="flex items-center space-x-7">
-    <img src={TwitterIcon} alt=""/>
+    <FooterIcon src={TwitterIcon}/>
+    <FooterIcon src={TwitterIcon}/>
+    <FooterIcon src={TwitterIcon}/>
+    <FooterIcon src={TwitterIcon}/>
+    <FooterIcon src={TwitterIcon}/>
     <img src={MediumIcon} alt=""/>
     <img src={DocsIcon} alt=""/>
     <img src={DiscordIcon} alt=""/>
