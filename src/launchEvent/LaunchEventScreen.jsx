e7ab6ab7325fcade6ec0e02020d50079a440844b
Itay Levy <itaylevy134@gmail.com>
2022-01-24 11:19:53 +0200

    Now the LaunchEventScreen.jsx looks perfect

--- src/launchEvent/LaunchEventScreen.jsx
+++ src/launchEvent/LaunchEventScreen.jsx
@@ -4,15 +4,16 @@ import {Footer} from './Footer'
 
 export const LaunchEventScreen = () => {
     return <ScreenPaddedContainer>
-        <div className="flex flex-col items-center w-full max-w-screen-xl h-full justify-between">
-            <Header/>
+        <div className="flex flex-col items-center w-full max-w-screen-xl h-full">
+            <Header className="mb-32"/>
             <Body/>
+            <div className="flex-1"/>
             <Footer/>
         </div>
     </ScreenPaddedContainer>
 }
 
 export const ScreenPaddedContainer = (props) => <div
-    className="w-screen h-screen bg-black flex flex-col items-center px-40 py-10">
+    className="w-screen h-screen bg-black flex flex-col items-center px-40 pt-10 pb-16">
     {props.children}
 </div>
