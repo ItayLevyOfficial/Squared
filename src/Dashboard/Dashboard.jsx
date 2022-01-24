eb222f6910acead16486dc3bd9b527f566ec8113
Itay Levy <itaylevy134@gmail.com>
2022-01-24 11:59:55 +0200

    Reformatted the ModalDisplay.jsx

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import Footer from '../Layouts/Footer'
 import Header from '../Layouts/Header'
 import ProductPool from '../Products/LiquidityPool'
 import Balance from './Balance'
 import Sidenav from '../Layouts/Sidenav'
-import { StakingPoolsObject } from '../Products/StakingPools'
+import {StakingPoolsObject} from '../Products/StakingPools'
 import ModalDisplay from '../Products/ModalDisplay'
 
 const Dashboard = () => {
@@ -22,21 +22,21 @@ const Dashboard = () => {
   return (
     <div className="flex">
       <ModalDisplay isOpen={isModalOpen} close={close} selectedToken={selectedToken}/>
-      <Sidenav />
+      <Sidenav/>
       <div className="flex flex-col w-10/12 max-w-main min-h-full">
-        <Header />
-        <Balance />
+        <Header/>
+        <Balance/>
         <div className="w-full">
           <span className="text-white text-3xl opacity-90">
             Liquidity Pools
           </span>
-          <div className="border-b w-full border-primary opacity-80 mb-4" />
+          <div className="border-b w-full border-primary opacity-80 mb-4"/>
 
           {StakingPoolsObject.map((el) => (
-            <ProductPool el={el} key={el.id} openModal={() => open(el.id)} />
+            <ProductPool el={el} key={el.id} openModal={() => open(el.id)}/>
           ))}
         </div>
-        <Footer />
+        <Footer/>
       </div>
     </div>
   )
--- src/Products/ModalDisplay.jsx
+++ src/Products/ModalDisplay.jsx
@@ -10,73 +10,73 @@ import {StakingPoolsObject} from './StakingPools'
 Modal.setAppElement('#root')
 
 const contentStyles = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    border: '1px solid #ccc',
-    background: `black`,
-    borderRadius: '10px',
-    borderColor: 'white',
-    overflow: 'auto',
-    WebkitOverflowScrolling: 'touch',
-    outline: 'none',
-    width: '600px',
-    height: '500px',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  right: 'auto',
+  bottom: 'auto',
+  marginRight: '-50%',
+  transform: 'translate(-50%, -50%)',
+  border: '1px solid #ccc',
+  background: `black`,
+  borderRadius: '10px',
+  borderColor: 'white',
+  overflow: 'auto',
+  WebkitOverflowScrolling: 'touch',
+  outline: 'none',
+  width: '600px',
+  height: '500px',
 }
 
 const overlayStyles = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100vw',
-    height: '100vh',
-    backgroundColor: 'rgba(0, 0, 0, 0.70)',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  width: '100vw',
+  height: '100vh',
+  backgroundColor: 'rgba(0, 0, 0, 0.70)',
 }
 
 const DisplayModal = ({isOpen, close, selectedToken}) => {
-    const [isOnWithdraw, setIsOnWithdraw] = useState(false)
-    const obj = StakingPoolsObject.find((el) => el.id === selectedToken)
+  const [isOnWithdraw, setIsOnWithdraw] = useState(false)
+  const obj = StakingPoolsObject.find((el) => el.id === selectedToken)
 
-    return (
-        <Modal
-            isOpen={isOpen}
-            onRequestClose={close}
-            style={{
-                overlay: overlayStyles,
-                content: contentStyles,
-            }}
-        >
-            <div className="flex justify-end">
-                <button onClick={close}>
-                    <XIcon className="h-6 text-white"/>
-                </button>
-            </div>
-            <div className="text-2xl mb-4 -mt-4 text-white flex justify-center">
-                {obj?.title}
-            </div>
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={close}
+      style={{
+        overlay: overlayStyles,
+        content: contentStyles,
+      }}
+    >
+      <div className="flex justify-end">
+        <button onClick={close}>
+          <XIcon className="h-6 text-white"/>
+        </button>
+      </div>
+      <div className="text-2xl mb-4 -mt-4 text-white flex justify-center">
+        {obj?.title}
+      </div>
 
-            <div className="border-white border rounded-lg bg-transparent w-full h-96 flex flex-col overflow-hidden">
-                <ModalOptions
-                    isOnWithdraw={isOnWithdraw}
-                    setIsOnWithdraw={setIsOnWithdraw}
-                    selectedToken={obj?.title}
-                />
-                <ModalInput selectedToken={obj?.title}/>
-                <ModalInfo selectedToken={obj?.title} isOnWithdraw={isOnWithdraw}/>
-                <br/>
+      <div className="border-white border rounded-lg bg-transparent w-full h-96 flex flex-col overflow-hidden">
+        <ModalOptions
+          isOnWithdraw={isOnWithdraw}
+          setIsOnWithdraw={setIsOnWithdraw}
+          selectedToken={obj?.title}
+        />
+        <ModalInput selectedToken={obj?.title}/>
+        <ModalInfo selectedToken={obj?.title} isOnWithdraw={isOnWithdraw}/>
+        <br/>
 
-                <ModalButtons
-                    isOnWithdraw={isOnWithdraw}
-                    selectedToken={obj?.title}
-                />
-                <br/>
-            </div>
-        </Modal>
-    )
+        <ModalButtons
+          isOnWithdraw={isOnWithdraw}
+          selectedToken={obj?.title}
+        />
+        <br/>
+      </div>
+    </Modal>
+  )
 }
 
 export default DisplayModal
