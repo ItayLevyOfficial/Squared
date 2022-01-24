bd639e4c265fa0ce8ed64186581788bd9537c737
Itay Levy <itaylevy134@gmail.com>
2022-01-24 12:00:39 +0200

    reformatted app

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,21 @@ import {LaunchEventScreen} from "./launchEvent/LaunchEventScreen"
 import {AccountStatus} from './launchEvent/accountStatus'
 
 function App() {
-    return (
-        <div className="flex justify-center w-full">
-            <Router>
-                <ScrollToTop>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/launch" element={<LaunchEventScreen/>}/>
-                        <Route path="/dashboard" element={<Dashboard/>}/>
-                        <Route path="/products" element={<Products/>}/>
-                        <Route path="/testing" element={<AccountStatus/>}/>
-                    </Routes>
-                </ScrollToTop>
-            </Router>
-        </div>
-    )
+  return (
+    <div className="flex justify-center w-full">
+      <Router>
+        <ScrollToTop>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/launch" element={<LaunchEventScreen/>}/>
+            <Route path="/dashboard" element={<Dashboard/>}/>
+            <Route path="/products" element={<Products/>}/>
+            <Route path="/testing" element={<AccountStatus/>}/>
+          </Routes>
+        </ScrollToTop>
+      </Router>
+    </div>
+  )
 }
 
 export default App
