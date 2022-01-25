2b9a89e194186ebe6a2b7397472298f8abbc9598
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-01-25 10:53:04 +0200

    change launch dragon z-index

--- src/launchEvent/Body.jsx
+++ src/launchEvent/Body.jsx
@@ -1,17 +1,26 @@
-import {AccountStatus} from './AccountStatus'
-import {EventStatus} from './EventStatus'
-import {useState} from 'react'
+import { AccountStatus } from './AccountStatus'
+import { EventStatus } from './EventStatus'
+import { useState } from 'react'
 import ModalDisplay from '../Products/ModalDisplay'
 
-export const Body = ({className}) => {
+export const Body = ({ className }) => {
   const [selectedToken, setSelectedToken] = useState(null)
 
-  return <div className={`flex space-x-32 ${className}`}>
-    <AccountStatus bnbCommitted={57} busdCommitted={93} handleBnbClick={() => setSelectedToken(2)}
-                   handleBusdClick={() => setSelectedToken(1)}/>
-    <div className="w-[0.5px] h-full bg-white"/>
-    <EventStatus/>
-    <ModalDisplay isOpen={selectedToken} selectedToken={selectedToken ?? ''}
-                  close={() => setSelectedToken(null)}/>
-  </div>
+  return (
+    <div className={`flex z-10 space-x-32 ${className}`}>
+      <AccountStatus
+        bnbCommitted={57}
+        busdCommitted={93}
+        handleBnbClick={() => setSelectedToken(2)}
+        handleBusdClick={() => setSelectedToken(1)}
+      />
+      <div className="w-[0.5px] h-full bg-white" />
+      <EventStatus />
+      <ModalDisplay
+        isOpen={selectedToken}
+        selectedToken={selectedToken ?? ''}
+        close={() => setSelectedToken(null)}
+      />
+    </div>
+  )
 }
--- src/launchEvent/LaunchEventScreen.jsx
+++ src/launchEvent/LaunchEventScreen.jsx
@@ -1,21 +1,24 @@
-import {Header} from "./Header";
-import {Body} from './Body'
-import {Footer} from './Footer'
-import Dragon from './icons/dragon.png';
+import { Header } from './Header'
+import { Body } from './Body'
+import { Footer } from './Footer'
+import Dragon from './icons/dragon.png'
 
 export const LaunchEventScreen = () => {
-  return <ScreenPaddedContainer>
-    <div className="flex flex-col items-center w-full max-w-screen-xl h-full">
-      <Header className="mb-28"/>
-      <Body/>
-      <div className="flex-1"/>
-      <Footer/>
-    </div>
-    <img src={Dragon} alt='' className='fixed bottom-6 left-6'/>
-  </ScreenPaddedContainer>
+  return (
+    <ScreenPaddedContainer>
+      <div className="flex flex-col items-center w-full max-w-screen-xl h-full">
+        <Header className="mb-28" />
+        <Body />
+        <div className="flex-1" />
+        <Footer />
+      </div>
+      <img src={Dragon} alt="" className="fixed bottom-6 left-6 z-0" />
+    </ScreenPaddedContainer>
+  )
 }
 
-export const ScreenPaddedContainer = (props) => <div
-  className="w-screen h-screen bg-black flex flex-col items-center px-40 pt-10 pb-16">
-  {props.children}
-</div>
+export const ScreenPaddedContainer = (props) => (
+  <div className="w-screen h-screen bg-black flex flex-col items-center px-40 pt-10 pb-16">
+    {props.children}
+  </div>
+)
