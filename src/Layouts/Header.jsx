dc1ab317335f86f4cb102eb8da4a6a513d6170b5
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-02-06 11:46:32 -0500

    add connect wallet

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,5 +1,53 @@
 import { useLocation } from 'react-router-dom'
 import { useState } from 'react'
+import { ethers } from 'ethers'
+
+export const AddressButton = () => {
+  const [isHovered, setIsHovered] = useState(false)
+  const [address, setAddress] = useState('Connect Wallet')
+
+  const connectWalletHandler = async () => {
+    try {
+      await ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x7a' }],
+      })
+    } catch (switchError) {
+      if (switchError.code === 4902) {
+        try {
+          await ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: '0x7a',
+                chainName: 'Fuse',
+                rpcUrls: ['https://rpc.fuse.io/'],
+              },
+            ],
+          })
+        } catch (addError) {
+          console.error(addError)
+        }
+      }
+    }
+    const provider = new ethers.providers.Web3Provider(window.ethereum, 'any')
+    await provider.send('eth_requestAccounts', [])
+    const signer = provider.getSigner()
+    setAddress(await signer.getAddress())
+  }
+
+  return (
+    <button
+      onClick={connectWalletHandler}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="border-solid border-[0.5px] border-white text-white font-number hover:font-baloo h-fit
+                    px-5 py-2 rounded-xl hover:border-0 hover:bg-lightDark"
+    >
+      {isHovered ? 'Disconnect' : address.toString().substring(0, 20)}
+    </button>
+  )
+}
 
 export const Header = () => {
   const { pathname } = useLocation()
@@ -11,22 +59,5 @@ export const Header = () => {
       </div>
       <AddressButton />
     </nav>
-  )
-}
-
-export const AddressButton = ({
-  address = '0xA3318B6027DC8fC382F990Bee9d2308E2ea3a388',
-}) => {
-  const [isHovered, setIsHovered] = useState(false)
-
-  return (
-    <button
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      className="border-solid border-[0.5px] border-white text-white font-number hover:font-baloo h-fit
-                    px-5 py-2 rounded-xl hover:border-0 hover:bg-lightDark"
-    >
-      {isHovered ? 'Disconnect' : `${address.slice(0, 7)}...`}
-    </button>
   )
 }
