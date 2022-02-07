a02c63e17747bd089b21a0faf237caa7fd3c7466
The dragon <mrbeesizzle@gmail.com>
2022-02-07 22:19:12 +0200

    changed the tabsize to two

--- src/launchEvent/LaunchEventScreen.jsx
+++ src/launchEvent/LaunchEventScreen.jsx
@@ -7,6 +7,7 @@ import { useConnectWallet } from './useConnectWallet';
 
 export const LaunchEventScreen = () => {
   const [signer, connectWallet, address] = useConnectWallet()
+  
 
   return (
     <ScreenPaddedContainer>
