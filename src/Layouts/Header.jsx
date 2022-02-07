07559bd4eb9884cb86dbcd1040ab0af1f69a3ede
The dragon <mrbeesizzle@gmail.com>
2022-02-07 12:15:52 +0200

    keep working

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,5 +1,6 @@
 import { useLocation } from 'react-router-dom'
 import { useState } from 'react'
+import { ethers } from 'ethers'
 import React from 'react'
 
 export const Header = () => {
@@ -15,8 +16,38 @@ export const Header = () => {
   )
 }
 
-export const AddressButton = ({ address }) => {
+export const AddressButton = () => {
   const [isHovered, setIsHovered] = useState(false)
+  const [address, setAddress] = useState()
+  const connectWalletHandler = async () => {
+    try {
+      await ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x7a' }],
+      })
+    } catch (switchError) {
+      if (switchError.code === 4902) {
+        try {
+          await ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: '0x7a',
+                chainName: 'Fuse',
+                rpcUrls: ['https://rpc.fuse.io/'],
+              },
+            ],
+          })
+        } catch (addError) {
+          console.error(addError)
+        }
+      }
+    }
+    const provider = new ethers.providers.Web3Provider(window.ethereum, 'any')
+    await provider.send('eth_requestAccounts', [])
+    const signer = provider.getSigner()
+    setAddress(await signer.getAddress())
+  }
 
   const buttonStyles = 'bg-primary border-none font-baloo'
   const styles = address ? (isHovered ? buttonStyles : '') : buttonStyles
@@ -29,6 +60,7 @@ export const AddressButton = ({ address }) => {
 
   return (
     <button
+      onClick={connectWalletHandler}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       className={`border-solid border-[0.5px] border-white text-white font-number h-fit px-5 py-2 rounded-xl ${styles}`}
