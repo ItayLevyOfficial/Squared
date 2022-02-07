f3b78f8c08464ef4112de5abfe26eb1e3826def1
Itay <itaylevy134@gmail.com>
2022-02-07 11:41:30 +0200

    Feat/pre connect launch (#13)
    Implemented the pre connect wallet launch event screen

--- src/launchEvent/Body.jsx
+++ src/launchEvent/Body.jsx
@@ -1,25 +1,29 @@
 import { AccountStatus } from './AccountStatus'
 import { EventStatus } from './EventStatus'
 import { useState } from 'react'
-import { ModalDisplay } from '../products/ModalDisplay'
+import React from 'react'
+import { CommitAssetsModal } from './commitAssetsModal';
 
 export const Body = ({ className }) => {
   const [selectedToken, setSelectedToken] = useState(null)
-
+  const [isConnected, setIsConnected] = useState(false)
+  const [nativeCommitted, setNativeCommitted] = useState(0)
+  const [stableCommitted, setStableCommitted] = useState(0)
+  
   return (
     <div className={`flex space-x-32 -mt-20 ${className}`}>
       <AccountStatus
-        bnbCommitted={57}
-        busdCommitted={93}
+        nativeCommitted={nativeCommitted}
+        stableCommitted={stableCommitted}
         handleBnbClick={() => setSelectedToken(2)}
         handleBusdClick={() => setSelectedToken(1)}
       />
       <div className="w-[0.5px] h-full bg-white" />
       <EventStatus />
-      <ModalDisplay
+      <CommitAssetsModal
         isOpen={selectedToken}
         selectedToken={selectedToken ?? ''}
-        close={() => setSelectedToken(null)}
+        close={() => setSelectedToken(null)} isConnected={isConnected}
       />
     </div>
   )
