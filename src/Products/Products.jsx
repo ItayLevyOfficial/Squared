a9b2548a67e4ed572fad64a690ab0a79136803bc
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-02-08 12:53:59 +0200

    Feat/change footer (#18)
    Now the website has the new footer

--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,18 +1,13 @@
 import { useState } from 'react'
 import { StakingPool } from './StakingPool'
-import { Header } from '../layouts/Header'
-import { Footer } from '../layouts/Footer'
-import { ModalDisplay } from './ModalDisplay'
-import { Sidenav } from '../layouts/Sidenav'
 import { StakingPoolsObject } from './StakingPools'
 import { Information } from './Information'
-import React from 'react'
-import { useConnectWallet } from '../launchEvent/useConnectWallet';
+import { PageWrapper } from '../layouts/PageWrapper'
+import { ModalDisplay } from './ModalDisplay'
 
 export const Products = () => {
   const [isModalOpen, setIsOpen] = useState(false)
   const [selectedToken, setSelectedToken] = useState('')
-  const [signer, connectWallet, address] = useConnectWallet()
 
   const open = (id) => {
     setIsOpen(true)
@@ -24,26 +19,18 @@ export const Products = () => {
   }
 
   return (
-    <div className="flex">
+    <PageWrapper>
       <ModalDisplay
         isOpen={isModalOpen}
         close={close}
         selectedToken={selectedToken}
       />
-      <Sidenav />
-
-      <div className="flex flex-col w-10/12 max-w-main min-h-full">
-        <Header connectWallet={connectWallet} address={address}/>
-        <div className="mt-20">
-          <div className="w-full flex items-center justify-evenly overflow-x-scroll ">
-            {StakingPoolsObject.map((el) => (
-              <StakingPool el={el} key={el.id} openModal={() => open(el.id)} />
-            ))}
-          </div>
-          <Information />
-          <Footer />
-        </div>
+      <div className="w-full flex items-center justify-evenly overflow-x-scroll ">
+        {StakingPoolsObject.map((el) => (
+          <StakingPool el={el} key={el.id} openModal={() => open(el.id)} />
+        ))}
       </div>
-    </div>
+      <Information />
+    </PageWrapper>
   )
 }
