1bd547cf86148ea85724f0983787d78a5e7a9c2d
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-02-07 22:14:42 -0500

    add page wrapper

--- src/Layouts/PageWrapper.jsx
+++ src/Layouts/PageWrapper.jsx
@@ -1,36 +1,21 @@
-import { useState } from 'react'
+import React from 'react'
 import { Header } from './Header'
 import { Footer } from '../launchEvent/Footer'
 import { Sidenav } from './Sidenav'
-import { ModalDisplay } from '../products/ModalDisplay'
-import { useConnectWallet } from '../launchEvent/useConnectWallet'
+import {useConnectWallet} from '../launchEvent/useConnectWallet'
 
 export const PageWrapper = ({children}) => {
   const [signer, connectWallet, address] = useConnectWallet()
-  const [isModalOpen, setIsOpen] = useState(false)
-  const [selectedToken, setSelectedToken] = useState('')
-
-  const open = (id) => {
-    setIsOpen(true)
-    setSelectedToken(id)
-  }
-
-  const close = () => {
-    setIsOpen(false)
-  }
 
 return (
   <div className="flex">
-    <ModalDisplay
-      isOpen={isModalOpen}
-      close={close}
-      selectedToken={selectedToken}
-    />
       <Sidenav />
       <div className="flex flex-col w-10/12 max-w-main min-h-full">
         <Header connectWallet={connectWallet} address={address}/>
-          {children}
-        <div className="w-full flex justify-center h-full items-end mb-6">
+          <div className="flex flex-col self-center justify-center items-center w-full h-full">
+            {children}
+          </div>
+        <div className="w-full flex justify-center items-end mb-6">
           <Footer />
         </div>
       </div>
--- src/Products/Products.jsx
+++ src/Products/Products.jsx
@@ -1,19 +1,36 @@
+import { useState } from 'react'
 import { StakingPool } from './StakingPool'
 import { StakingPoolsObject } from './StakingPools'
 import { Information } from './Information'
 import { PageWrapper } from '../layouts/PageWrapper'
+import { ModalDisplay } from './ModalDisplay'
 
 export const Products = () => {
+  const [isModalOpen, setIsOpen] = useState(false)
+  const [selectedToken, setSelectedToken] = useState('')
+
+  const open = (id) => {
+    setIsOpen(true)
+    setSelectedToken(id)
+  }
+
+  const close = () => {
+    setIsOpen(false)
+  }
+
   return (
     <PageWrapper>
-      <div className="mt-20">
-        <div className="w-full flex items-center justify-evenly overflow-x-scroll ">
-          {StakingPoolsObject.map((el) => (
-            <StakingPool el={el} key={el.id} openModal={() => open(el.id)} />
-          ))}
-        </div>
-        <Information />
+      <ModalDisplay
+        isOpen={isModalOpen}
+        close={close}
+        selectedToken={selectedToken}
+      />
+      <div className="w-full flex items-center justify-evenly overflow-x-scroll ">
+        {StakingPoolsObject.map((el) => (
+          <StakingPool el={el} key={el.id} openModal={() => open(el.id)} />
+        ))}
       </div>
+      <Information />
     </PageWrapper>
   )
 }
