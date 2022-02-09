763bd362e33555c5571bfc01e68a1bd66dfe1de2
The dragon <mrbeesizzle@gmail.com>
2022-02-09 18:55:35 +0200

    Now the modalinput component looks great

--- src/Products/ModalInput.jsx
+++ src/Products/ModalInput.jsx
@@ -11,14 +11,19 @@ export const ModalInput = ({
     <div
       className={`self-center w-10/12 h-10 bg-dark flex justify-between items-center rounded-lg p-6 ${className} `}
     >
-      <button className="px-2 -ml-4 h-6 flex justify-center items-center border-transparent bg-primary text-white hover:bg-opacity-90 rounded-lg">
+      <button onClick={handleMaxClick}
+        className="px-2 -ml-4 h-6 flex justify-center items-center border-transparent
+       bg-primary text-white hover:bg-opacity-90 rounded-lg"
+      >
         MAX
       </button>
       <input
         type="number"
         className="w-full  bg-dark mx-2 text-white outline-none text-xl"
         autoFocus
+        onChange={(event) => handleChange(event.target.value)}
         dir="rtl"
+        value={value}
       />
       <div className=" text-white text-sm -mr-4 w-auto right-0 ">
         {selectedToken}
