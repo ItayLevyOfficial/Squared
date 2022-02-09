106db6a4e683f19e4f23da6f5b8a62aacd0b6b52
The dragon <mrbeesizzle@gmail.com>
2022-02-09 17:18:04 +0200

    keep on working

--- src/launchEvent/useContract.jsx
+++ src/launchEvent/useContract.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react'
 
 export const useContract = (signer, contractAddress, abi) => {
   const [contract, setContract] = useState()
 
   useEffect(() => {
     setContract(
-      new ethers.Contract(selectedChain.launchContractAddress, launchContractAbi, signer)
+      new ethers.Contract(
+        selectedChain.launchContractAddress,
+        launchContractAbi,
+        signer
+      )
     )
   }, [signer])
-
 }
