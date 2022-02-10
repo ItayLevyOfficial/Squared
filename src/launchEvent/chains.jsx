272a591b919fb7f12fee650743f48994fc2e4116
The dragon <mrbeesizzle@gmail.com>
2022-02-10 17:23:34 +0200

    updated the launch contract address

--- src/launchEvent/chains.jsx
+++ src/launchEvent/chains.jsx
@@ -10,7 +10,7 @@ const chains = {
     rpcUrls: ['http://127.0.0.1:8545/'],
     nativeToken: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
     stableToken: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-    launchContractAddress: '0xD42912755319665397FF090fBB63B1a31aE87Cee',
+    launchContractAddress: '0xb9bEECD1A582768711dE1EE7B0A1d582D9d72a6C',
     tokens: [
       {
         name: 'ETH',
