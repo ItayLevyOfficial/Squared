ad9c4e7f8c3989e5350e9b3a4eb8ab449e860fa0
The dragon <mrbeesizzle@gmail.com>
2022-02-15 15:32:44 -0500

    keep working

--- src/launchEvent/commitNotAllowed.jsx
+++ src/launchEvent/commitNotAllowed.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Modal } from 'react-modal'
 import { overlayStyles } from '../Products/ModalDisplay'
 import { commitContentStyles } from './commitAssetsModal'
+import { CloseButton } from '../Products/ModalDisplay'
 
 export const CommitsNotAllowed = ({
   isOpen,
@@ -18,7 +19,11 @@ export const CommitsNotAllowed = ({
         content: commitContentStyles,
       }}
     >
-      
+      <CloseButton close={close} />
+
+        <h1 className="text-2xl mb-8 -mt-4 text-white font-medium">
+          Commit {tokenData?.name}
+        </h1>
     </Modal>
   )
 }
