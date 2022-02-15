9ebf9b0308625c44db6dc2c0e0e0e9d77960a3fc
Itay <itaylevy134@gmail.com>
2022-02-14 20:47:11 -0500

    Feat/read balance (#23)
    Implemented the read balance feature great

--- src/launchEvent/Body.jsx
+++ src/launchEvent/Body.jsx
@@ -1,20 +1,49 @@
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { AccountStatus } from './AccountStatus'
 import { CommitAssetsModal } from './commitAssetsModal'
 import { EventStatus } from './EventStatus'
+import { useEffect } from 'react'
+import { ethers } from 'ethers'
+import { selectedChain } from './chains'
 
-export const Body = ({ className = '', launchContract }) => {
+export const Body = ({ className = '', launchContract, address }) => {
   const [selectedToken, setSelectedToken] = useState(null)
-  const [nativeCommitted, setNativeCommitted] = useState(0)
-  const [stableCommitted, setStableCommitted] = useState(0)
+  const [depositedToken, setDepositedToken] = useState(
+    ethers.constants.AddressZero
+  )
+  const [balance, setBalance] = useState(0)
+
+  const fetchBalance = useCallback(async () => {
+    const accountToken = await launchContract.accountToken(address)
+    setDepositedToken(accountToken)
+    if (accountToken !== ethers.constants.AddressZero) {
+      const newBalance = await launchContract.accountBalance(address)
+      const usdChainlinkDecimals = 8
+      const formattedNewBalance = newBalance.div(10 ** usdChainlinkDecimals)
+      setBalance(formattedNewBalance.toNumber())
+    }
+  }, [address, launchContract])
+
+  useEffect(() => {
+    if (launchContract && address) {
+      fetchBalance()
+    }
+  }, [address, fetchBalance, launchContract])
+
+  useEffect(() => {
+    if (launchContract) {
+      launchContract.on('Deposited', fetchBalance)
+      return () => launchContract.removeListener(fetchBalance)
+    }
+  }, [fetchBalance, launchContract])
 
   return (
     <div className={`flex space-x-32 -mt-20 ${className}`}>
       <AccountStatus
-        nativeCommitted={nativeCommitted}
-        stableCommitted={stableCommitted}
-        handleBnbClick={() => setSelectedToken(0)}
-        handleBusdClick={() => setSelectedToken(1)}
+        amountCommitted={balance}
+        isNativeCommitted={depositedToken === selectedChain.tokens[0].address}
+        handleNativeClick={() => setSelectedToken(0)}
+        handleStableClick={() => setSelectedToken(1)}
       />
       <div className="w-[0.5px] h-full bg-white" />
       <EventStatus />
