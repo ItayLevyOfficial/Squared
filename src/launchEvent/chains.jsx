5fda5b49dd693b3c608a21a610c9f1f72bdba53b
The dragon <mrbeesizzle@gmail.com>
2022-02-17 13:30:59 -0500

    Now the top bar squash great

--- src/launchEvent/chains.jsx
+++ src/launchEvent/chains.jsx
@@ -1,9 +1,10 @@
 import { EthereumIcon } from './icons/ethereum'
 import React from 'react'
 import { Usdc } from './icons/usdc'
+import { Bnb } from './icons/BNB'
+import { Busd } from './icons/busd'
 
 const localChainConfig = {
-  ethereum: {
     chainId: '0x539',
     chainName: 'hardhat',
     rpcUrls: ['http://127.0.0.1:8545/'],
@@ -34,7 +35,6 @@ const localChainConfig = {
         decimals: '6',
       },
     ],
-  },
 }
 
 const chains = {
@@ -47,44 +47,12 @@ const chains = {
   bsc: {
     ...localChainConfig,
     chainName: 'BSC',
-    tokens: [{}],
-  },
-  ethereum: {
-    chainId: '0x539',
-    chainName: 'hardhat',
-    rpcUrls: ['http://127.0.0.1:8545/'],
-    launchContractAddress: '0x8A93d247134d91e0de6f96547cB0204e5BE8e5D8',
-    launchTime: 1644612469136,
-    launchTokensAmount: 3_000_000,
     tokens: [
-      {
-        id: 0,
-        name: 'ETH',
-        address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
-        decimals: '18',
-        icon: <EthereumIcon />,
-      },
-      {
-        id: 1,
-        name: 'USDC',
-        address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-        decimals: '6',
-        icon: <Usdc />,
-      },
-      {
-        id: 2,
-        name: 'PUFF',
-        address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // replace with official address
-        decimals: '6',
-      },
-      {
-        id: 3,
-        name: 'PUFF VOLTAGE LP',
-        address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // replace with official address
-        decimals: '6',
-      },
+      { ...localChainConfig.tokens[0], name: 'BNB', icon: <Bnb /> },
+      { ...localChainConfig.tokens[1], name: 'BUSD', icon: <Busd /> },
     ],
   },
+  ethereum: localChainConfig,
 }
 
 export const selectedChain = chains.ethereum
