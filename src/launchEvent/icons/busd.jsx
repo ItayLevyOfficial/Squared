1635fd18a596c3b8abfd31c9f2dcfad513fcaff4
The dragon <mrbeesizzle@gmail.com>
2022-02-16 21:26:41 -0500

    now the bsc icons look great

--- src/launchEvent/icons/busd.jsx
+++ src/launchEvent/icons/busd.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { theme } from '../../theme';
 export const Busd = () => <svg
   width="30"
   height="31"
@@ -9,23 +10,23 @@ export const Busd = () => <svg
 <g clip-path="url(#clip0_31_375)">
 <path
       d="M14.9995 0.0633087L18.7004 3.85333L9.38138 13.1723L5.68054 9.47147L14.9995 0.0633087Z"
-      fill="#FBC403"
-      stroke="#FBC403"
+      fill={theme.darkPrimary}
+      stroke={theme.darkPrimary}
     />
 <path
       d="M20.6177 5.68146L24.3185 9.47148L9.38138 24.4086L5.68054 20.7078L20.6177 5.68146Z"
-      fill="#FBC403"
-      stroke="#FBC403"
+      fill={theme.darkPrimary}
+      stroke={theme.darkPrimary}
     />
 <path
       d="M3.76322 11.2996L7.46406 15.0896L3.76322 18.7905L0.0623779 15.0896L3.76322 11.2996Z"
-      fill="#FBC403"
-      stroke="#FBC403"
+      fill={theme.darkPrimary}
+      stroke={theme.darkPrimary}
     />
 <path
       d="M26.2358 11.2996L29.9367 15.0896L14.9995 30.0267L11.2987 26.3259L26.2358 11.2996Z"
-      fill="#FBC403"
-      stroke="#FBC403"
+      fill={theme.darkPrimary}
+      stroke={theme.darkPrimary}
     />
 </g>
 <defs>
