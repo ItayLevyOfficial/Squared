e0d13a740bb45f9077d9c765433d825ebbbf6a86
The dragon <mrbeesizzle@gmail.com>
2022-02-16 21:33:41 -0500

    added the ethereum icon

---
+++ src/launchEvent/icons/ethereum.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export const EthereumIcon = () => (
+  <svg
+    width="654"
+    height="1063"
+    viewBox="0 0 654 1063"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      opacity="0.6"
+      d="M327.064 392.732L0.983398 541.033L327.064 733.682L653.017 541.033L327.064 392.732Z"
+      fill="white"
+    />
+    <path
+      d="M0.983398 541.033L327.064 733.682V0L0.983398 541.033Z"
+      fill="white"
+    />
+    <path d="M327.064 0V733.682L653.017 541.033L327.064 0Z" fill="white" />
+    <path
+      d="M0.983398 602.813L327.064 1062.2V795.463L0.983398 602.813Z"
+      fill="white"
+    />
+    <path
+      d="M327.064 795.463V1062.2L653.273 602.813L327.064 795.463Z"
+      fill="white"
+    />
+  </svg>
+)
