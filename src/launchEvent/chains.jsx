e345807b9e85197ae1c99ee4cc1ebab857084c80
The dragon <mrbeesizzle@gmail.com>
2022-02-16 21:40:29 -0500

    added the icons to the chain object

--- src/launchEvent/chains.jsx
+++ src/launchEvent/chains.jsx
@@ -1,5 +1,6 @@
-import { EthereumIcon } from "./icons/ethereum"
-import React from "react"
+import { EthereumIcon } from './icons/ethereum'
+import React from 'react'
+import { Usdc } from './icons/usdc'
 const chains = {
   fuse: {
     chainId: '0x7a',
@@ -19,13 +20,14 @@ const chains = {
         name: 'ETH',
         address: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
         decimals: '18',
-        icon: <EthereumIcon/>
+        icon: <EthereumIcon />,
       },
       {
         id: 1,
         name: 'USDC',
         address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
         decimals: '6',
+        icon: <Usdc />,
       },
       {
         id: 2,
