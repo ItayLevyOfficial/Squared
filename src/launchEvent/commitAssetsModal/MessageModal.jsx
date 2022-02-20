eafd921292c870ff498b3f8fe92284213e336b68
The dragon <mrbeesizzle@gmail.com>
2022-02-20 14:44:05 -0500

    added a great error title

--- src/launchEvent/commitAssetsModal/MessageModal.jsx
+++ src/launchEvent/commitAssetsModal/MessageModal.jsx
@@ -25,9 +25,9 @@ export const MessageModal = ({
     >
       <CloseButton close={close} />
       <div className="flex flex-col items-center">
-        <h1 className='text-2xl font-medium mt-2'>{header}</h1>
+        <h1 className='text-2xl font-medium mt-2 mb-6'>{header}</h1>
         {icon}
-        <p className="text-center mt-8 w-5/6 text-lg text-white font-medium">
+        <p className="text-center mt-6 w-5/6 text-lg text-white font-medium">
           In the launch event, you can only deposit either {depositedTokenName}{' '}
           or {tokenName}. Since you already committed {depositedTokenName},
           further deposits of {tokenName} are not allowed.{' '}
