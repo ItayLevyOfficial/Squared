cb4480ebac0003a94b90346faf909642d15462dd
The dragon <mrbeesizzle@gmail.com>
2022-02-20 18:10:54 -0500

    now the tx hash getting copied great

--- src/launchEvent/Body.jsx
+++ src/launchEvent/Body.jsx
@@ -1,26 +1,30 @@
-import React, { useCallback, useState } from 'react'
-import { AccountStatus } from './AccountStatus'
-import { CommitAssetsModal } from './commitAssetsModal/commitAssetsModal'
-import { EventStatus } from './EventStatus'
-import { useEffect } from 'react'
-import { ethers } from 'ethers'
-import { selectedChain } from '../chains'
-import { MessageModal } from './commitAssetsModal/MessageModal'
-import { useContract } from './utils'
-import { launchContractAbi } from './abis/defiRoundAbi'
-import { provider } from './useConnectWallet'
-import { SuccessIcon } from './icons/success'
-import { PrimaryLink } from './commitAssetsModal/commitAssetsModal'
-export const formatBigUsd = (bigUsd) => bigUsd.div(10 ** 8).toNumber()
+import { faCopy } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee, faCopy } from '@fortawesome/free-solid-svg-icons'
+import { ethers } from 'ethers'
+import React, { useCallback, useEffect, useState } from 'react'
+import { selectedChain } from '../chains'
+import { launchContractAbi } from './abis/defiRoundAbi'
+import { AccountStatus } from './AccountStatus'
+import {
+  CommitAssetsModal,
+  PrimaryLink,
+} from './commitAssetsModal/commitAssetsModal'
+import { MessageModal } from './commitAssetsModal/MessageModal'
+import { EventStatus } from './EventStatus'
+import { SuccessIcon } from './icons/success'
+import { provider } from './useConnectWallet'
+import { useContract } from './utils'
+import copy from 'copy-to-clipboard'
+export const formatBigUsd = (bigUsd) => bigUsd.div(10 ** 8).toNumber()
 
 export const Body = ({ className = '', writeLaunchContract, address }) => {
   const [selectedTokenIndex, setSelectedToken] = useState(null)
   const [depositedToken, setDepositedToken] = useState(
     ethers.constants.AddressZero
   )
-  const [transactionHash, setTransactionHash] = useState('0xe8195739bc5b4e4ea947ff9cb66befdf203418e937d097b0c6faf100d4390dad')
+  const [transactionHash, setTransactionHash] = useState(
+    '0xe8195739bc5b4e4ea947ff9cb66befdf203418e937d097b0c6faf100d4390dad'
+  )
   const [balance, setBalance] = useState(0)
 
   const readLaunchContract = useContract(
@@ -75,13 +79,19 @@ export const Body = ({ className = '', writeLaunchContract, address }) => {
           close={() => setTransactionHash(null)}
           footer={
             <>
-              Transactions on the {selectedChain.chainName} chain usually completed
-              within {selectedChain.approvalTime} minutes. Your transaction hash
-              is{'  '}
-              <span className="font-number bg-dark py-1 px-2 text-sm rounded-lg cursor-pointer">
-                {transactionHash.slice(0, 7)}... &nbsp;<FontAwesomeIcon icon={faCopy}/>
-              </span>{'  '}
-              and you can check its status on <PrimaryLink>Etherscan</PrimaryLink>.
+              Transactions on the {selectedChain.chainName} chain usually
+              completed within {selectedChain.approvalTime} minutes. Your
+              transaction hash is{'  '}
+              <span
+                className="font-number bg-dark py-1 px-2 text-sm rounded-lg cursor-pointer"
+                onClick={() => copy(transactionHash)}
+              >
+                {transactionHash.slice(0, 7)}... &nbsp;
+                <FontAwesomeIcon icon={faCopy} />
+              </span>
+              {'  '}
+              and you can check its status on{' '}
+              <PrimaryLink>Etherscan</PrimaryLink>.
             </>
           }
         />
