1164d5424460aa35c44f48316ad15b0edaa85cd9
The dragon <mrbeesizzle@gmail.com>
2022-02-20 21:43:39 -0500

    Now the success modal should work great

--- src/launchEvent/commitAssetsModal/MessageModal.jsx
+++ src/launchEvent/commitAssetsModal/MessageModal.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import { faCheck, faCopy } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { copy } from 'copy-to-clipboard'
+import React, { useState } from 'react'
 import Modal from 'react-modal'
-import { overlayStyles } from '../../Products/ModalDisplay'
-import { commitContentStyles } from './commitAssetsModal'
-import { CloseButton } from '../../Products/ModalDisplay'
+import { selectedChain } from '../../chains'
+import { CloseButton, overlayStyles } from '../../Products/ModalDisplay'
 import errorIcon from '../icons/error.svg'
-import { PrimaryLink } from './commitAssetsModal'
+import { SuccessIcon } from '../icons/success'
+import { commitContentStyles, PrimaryLink } from './commitAssetsModal'
 
 export const MessageModal = ({
   icon = <img src={errorIcon} alt="" />,
@@ -29,5 +32,41 @@ export const MessageModal = ({
         <p className="text-center mt-6 w-5/6 text-lg text-white">{footer}</p>
       </div>
     </Modal>
+  )
+}
+
+export const SuccessModal = ({ txHash, close }) => {
+  const [txCopied, setTxCopied] = useState(false)
+
+  return (
+    <MessageModal
+      icon={<SuccessIcon />}
+      header="Request Sent Successfully"
+      isOpen={Boolean(txHash)}
+      close={close}
+      footer={
+        <>
+          Transactions on the {selectedChain.chainName} chain are usually
+          completed within {selectedChain.approvalTime} minutes. Your
+          transaction hash is{'  '}
+          <span
+            className="font-number bg-dark py-1 px-2 text-sm rounded-lg cursor-pointer"
+            onClick={() => {
+              copy(txHash)
+              setTxCopied(true)
+            }}
+          >
+            {txHash.slice(0, 7)}... &nbsp;
+            <FontAwesomeIcon icon={txCopied ? faCheck : faCopy} />
+          </span>
+          {'  '}
+          and you can check its status on{' '}
+          <PrimaryLink onClick={() => window.open(selectedChain.scan.url)}>
+            {selectedChain.scan.name}
+          </PrimaryLink>
+          .
+        </>
+      }
+    />
   )
 }
