a7c1ed9c6977b67786a00093d85dc709427c9592
The dragon <mrbeesizzle@gmail.com>
2022-02-20 22:47:02 -0500

    Now my code look way better

--- src/launchEvent/LaunchEventScreen.jsx
+++ src/launchEvent/LaunchEventScreen.jsx
@@ -1,33 +1,17 @@
 import React from 'react'
 import { Body } from './Body'
-import { selectedChain } from '../chains'
-import { launchContractAbi } from './abis/defiRoundAbi'
 import { Footer } from './Footer'
 import { LaunchScreenHeader } from './LaunchScreenHeader'
-import { useConnectWallet } from './useConnectWallet'
-import { useContract } from './utils'
 
-export const LaunchEventScreen = () => {
-  const [signer, connectWallet, walletAddress] = useConnectWallet()
-  const writeLaunchContract = useContract(
-    signer,
-    selectedChain.launchContractAddress,
-    launchContractAbi
-  )
-  
-  return (
-    <ScreenPaddedContainer>
-      <div className="flex flex-col items-center justify-between w-full max-w-screen-2xl h-full">
-        <LaunchScreenHeader
-          address={walletAddress}
-          connectWallet={connectWallet}
-        />
-        <Body address={walletAddress}/>
-        <Footer />
-      </div>
-    </ScreenPaddedContainer>
-  )
-}
+export const LaunchEventScreen = () => (
+  <ScreenPaddedContainer>
+    <div className="flex flex-col items-center justify-between w-full max-w-screen-2xl h-full">
+      <LaunchScreenHeader />
+      <Body />
+      <Footer />
+    </div>
+  </ScreenPaddedContainer>
+)
 
 export const ScreenPaddedContainer = (props) => (
   <div className="w-screen h-screen bg-black flex flex-col items-center px-16 pt-10 pb-16">
