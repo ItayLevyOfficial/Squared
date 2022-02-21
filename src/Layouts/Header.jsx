1088b358b5c5a66366e72b249fff425ca51fb27a
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-02-21 11:41:19 -0500

    rm sidenav

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -1,12 +1,58 @@
 import MetamaskIcon from '../launchEvent/icons/metamask.svg'
 import React from 'react'
+import { LibraryIcon, BookOpenIcon } from '@heroicons/react/outline'
+import { useLocation } from 'react-router-dom'
+import { BrandingSection } from '../launchEvent/LaunchScreenHeader'
+
+export const PageToggle = () => {
+  const { pathname } = useLocation()
+
+  return (
+    <div className="pt-12 space-y-6 flex items-center">
+      <div className="flex cursor-pointer group space-x-2 items-center ">
+        <LibraryIcon
+          className={`h-6 text-white ${pathname !== '/assets' && 'opacity-40'}`}
+        />
+
+        <a
+          href="/assets"
+          className={`text-white font-medium  text-lg ${
+            pathname !== '/assets' && 'opacity-40'
+          }`}
+        >
+          Assets
+        </a>
+      </div>
+
+      <div className="flex cursor-pointer group space-x-2 items-center">
+        <BookOpenIcon
+          className={`h-6 text-white ${
+            pathname !== '/dashboard' && 'opacity-40'
+          }`}
+        />
+        <a
+          href="/dashboard"
+          className={`text-white font-medium text-lg ${
+            pathname !== '/dashboard' && 'opacity-40'
+          }`}
+        >
+          Dashboard
+        </a>
+      </div>
+    </div>
+  )
+}
 
 export const Header = (props) => {
   return (
-    <nav className="flex flex-wrap h-20 items-center justify-end w-full px-4 mt-7">
-      <img src={MetamaskIcon} alt="" className="mr-4" />
+    <nav className="flex flex-wrap h-20 items-center justify-between w-full px-4 mt-7">
+      <BrandingSection children={'CYCLE ZERO'} />
+      <PageToggle />
 
-      <AddressButton {...props} />
+      <div className="flex items-center h-fit w-44 justify-end flex-shrink-0">
+        <img src={MetamaskIcon} alt="" className="mr-4" />
+        <AddressButton {...props} />
+      </div>
     </nav>
   )
 }
--- src/Layouts/PageWrapper.jsx
+++ src/Layouts/PageWrapper.jsx
@@ -9,7 +9,6 @@ export const PageWrapper = ({ children }) => {
 
   return (
     <div className="w-screen h-screen bg-black flex px-16 ">
-      <Sidenav />
       <div className="flex flex-col w-full ">
         <Header connectWallet={connectWallet} address={address} />
         <div className="flex flex-col justify-evenly items-center w-full h-full ">
