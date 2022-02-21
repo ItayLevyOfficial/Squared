db1e54589fdfabcd30821a1b8b2faf0fbae43c20
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-02-20 19:02:49 -0500

    rm brandingsection duplicate

--- src/Layouts/Sidenav.jsx
+++ src/Layouts/Sidenav.jsx
@@ -1,25 +1,6 @@
 import { LibraryIcon, BookOpenIcon } from '@heroicons/react/outline'
 import { useLocation } from 'react-router-dom'
-import Logo from '../launchEvent/icons/logo.svg'
-
-const BrandingSection = () => (
-  <div className="flex flex-none h-fit">
-    <img src={Logo} width={60} />
-    <div className="w-5 flex-none" />
-    <div className="w-[0.5px] h-50 bg-primary float-none" />
-    <div className="w-5 float-none" />
-    <div className="flex flex-col space-y-2 font-bold tracking-widest">
-      <h1 className="text-white text-4xl font-basic font-semibold tracking-wide">
-        SQUARED
-      </h1>
-      <div className="flex items-center">
-        <h2 className="text-primary text-lg font-normal mr-1.5">
-          CYCLE ZERO-0
-        </h2>
-      </div>
-    </div>
-  </div>
-)
+import { BrandingSection } from '../launchEvent/LaunchScreenHeader'
 
 export const Sidenav = () => {
   const { pathname } = useLocation()
@@ -29,7 +10,7 @@ export const Sidenav = () => {
       <div className="h-10" />
       <div className="flex flex-col justify-between h-full">
         <div className="h-full space-y-6 items-start px-6 rounded-2xl">
-          <BrandingSection />
+          <BrandingSection children={'CYCLE ZERO-0'} />
 
           <div className="pt-12 space-y-6">
             <div className="flex cursor-pointer group space-x-2 items-center ">
