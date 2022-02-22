c0d34b6eef9280a0b870dbac4653a3ab29230d9e
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-02-22 12:16:36 -0500

    rm max

--- src/Products/ModalInput.jsx
+++ src/Products/ModalInput.jsx
@@ -11,24 +11,16 @@ export const ModalInput = ({
     <div
       className={`self-center w-10/12 h-10 bg-dark flex justify-between items-center rounded-lg p-6 ${className} `}
     >
-      <button
-        onClick={handleMaxClick}
-        className="px-2 -ml-4 h-6 flex justify-center items-center border-transparent
-       bg-darkPrimary text-white hover:bg-opacity-90 rounded-lg"
-      >
-        MAX
-      </button>
-      <input
-        type="number"
-        className="w-full  bg-dark mx-2 text-white outline-none text-xl"
-        autoFocus
-        onChange={(event) => handleChange(event.target.value)}
-        dir="rtl"
-        value={value}
-      />
-      <div className=" text-white text-sm -mr-4 w-auto right-0 ">
+      <div className=" text-white text-sm w-auto left-0 ">
         {selectedTokenIndex}
       </div>
+      <input
+        type="number"
+        className="w-full  bg-dark mx-4 text-white outline-none text-xl"
+        autoFocus
+        onChange={(event) => handleChange(event.target.value)}
+        value={value}
+      />
     </div>
   )
 }
