0d6035c5e584a8ad77c3a9c6329e965e53eedfc0
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-02-22 14:05:08 -0500

    add max button

--- src/Products/ModalInput.jsx
+++ src/Products/ModalInput.jsx
@@ -5,21 +5,30 @@ export const ModalInput = ({
   className = '',
   value,
   handleChange,
+  handleMaxClick,
 }) => {
   return (
     <div
       className={`self-center w-10/12 h-10 bg-dark flex justify-between items-center rounded-lg p-6 ${className} `}
     >
-      <div className=" text-white text-sm w-auto left-0 ">
+      <div className=" text-white text-sm  w-auto left-0 ">
         {selectedTokenIndex}
       </div>
       <input
         type="number"
-        className="w-full  bg-dark mx-4 text-white outline-none text-xl"
+        className="w-full bg-dark mr-6 ml-2 text-white outline-none text-xl"
         autoFocus
         onChange={(event) => handleChange(event.target.value)}
         value={value}
       />
+
+      <button
+        onClick={handleMaxClick}
+        className="px-2 -ml-4 h-6 flex justify-center items-center border-transparent
+       bg-darkPrimary text-white hover:bg-opacity-90 rounded-lg"
+      >
+        MAX
+      </button>
     </div>
   )
 }
