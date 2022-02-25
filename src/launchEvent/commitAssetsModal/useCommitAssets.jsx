a6504b0f159ae1dcade792c884d0f3ac73937e83
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-02-25 17:48:46 -0500

    add back commit assets

--- src/launchEvent/commitAssetsModal/useCommitAssets.jsx
+++ src/launchEvent/commitAssetsModal/useCommitAssets.jsx
@@ -2,7 +2,6 @@ import { BigNumber } from 'ethers'
 import { useContext, useState } from 'react'
 import { launchContractAbi } from '../abis/defiRoundAbi'
 import { StageContext } from '../LaunchEventScreen'
-import { EthPoolAbi } from '../../products/ABIs/EthPoolAbi'
 import { selectedChain } from '../../chains'
 import { erc20abi } from '../abis/erc20abi'
 import { useConnectWallet } from '../useConnectWallet'
@@ -49,5 +48,68 @@ export const useCommitPoolAssets = (selectedToken, abi) => {
       setTxHash(tx.hash)
     }
   }
+  return [commitAssets, txHash, setTxHash]
+}
+
+export const useCommitAssets = () => {
+  const [signer] = useConnectWallet()
+  const erc20 = useContract(signer, selectedChain.tokens[1].address, erc20abi)
+  const [txHash, setTxHash] = useState()
+  const launchContract = useContract(
+    signer,
+    selectedChain.launchData.launchContractAddress,
+    launchContractAbi
+  )
+  const launchStage = useContext(StageContext)
+
+  const commitAssets = async ({ tokenAmount, selectedTokenIndex }) => {
+    const tokenData = selectedChain.tokens[selectedTokenIndex]
+    const amount = parseNumberDecimals({
+      amount: tokenAmount,
+      decimals: tokenData.decimals,
+    })
+    if (launchStage === 1) {
+      if (selectedTokenIndex === 0) {
+        const tx = await launchContract.deposit(
+          {
+            token: tokenData.address,
+            amount: amount,
+          },
+          [],
+          { value: amount }
+        )
+        setTxHash(tx.hash)
+      } else {
+        await erc20.approve(
+          selectedChain.launchData.launchContractAddress,
+          amount
+        )
+        const tx = await launchContract.deposit(
+          {
+            token: tokenData.address,
+            amount: amount,
+          },
+          [],
+          {}
+        )
+        setTxHash(tx.hash)
+      }
+    } else {
+      if (selectedTokenIndex === 0) {
+        const tx = await launchContract.withdraw(
+          { token: tokenData.address, amount: amount },
+          true
+        )
+        setTxHash(tx.hash)
+      } else {
+        const tx = await launchContract.withdraw(
+          { token: tokenData.address, amount: amount },
+          false
+        )
+        setTxHash(tx.hash)
+      }
+    }
+  }
+
   return [commitAssets, txHash, setTxHash]
 }
