b4f9ac39b9c683327bf93ce8a26bef4b8d551ace
The dragon <mrbeesizzle@gmail.com>
2022-03-01 11:43:22 -0500

    moved commit launch assets to a separate fil;e

--- src/launchEvent/commitAssetsModal/useCommitAssets.jsx
+++ src/launchEvent/commitAssetsModal/useCommitAssets.jsx
@@ -1,12 +1,10 @@
 import { BigNumber } from 'ethers'
-import { useContext, useState } from 'react'
-import { launchContractAbi } from '../abis/defiRoundAbi'
-import { StageContext } from '../LaunchEventScreen'
+import { useState } from 'react'
 import { selectedChain } from '../../chains'
+import { usePoolContracts } from '../../products/useErc20Functions'
 import { erc20abi } from '../abis/erc20abi'
 import { useConnectWallet } from '../useConnectWallet'
 import { useContract } from '../utils'
-import { usePoolContracts } from '../../products/useErc20Functions'
 
 export const parseNumberDecimals = ({ amount, decimals }) => {
   const wholeSide = Math.floor(amount)
@@ -51,65 +49,4 @@ export const useCommitPoolAssets = (selectedToken, abi) => {
   return [commitAssets, txHash, setTxHash]
 }
 
-export const useCommitLaunchAssets = () => {
-  const [signer] = useConnectWallet()
-  const erc20 = useContract(signer, selectedChain.tokens[1].address, erc20abi)
-  const [txHash, setTxHash] = useState()
-  const launchContract = useContract(
-    signer,
-    selectedChain.launchData.launchContractAddress,
-    launchContractAbi
-  )
-  const launchStage = useContext(StageContext)
 
-  const commitAssets = async ({ tokenAmount, selectedTokenIndex }) => {
-    const tokenData = selectedChain.tokens[selectedTokenIndex]
-    const amount = parseNumberDecimals({
-      amount: tokenAmount,
-      decimals: tokenData.decimals,
-    })
-    if (launchStage === 1) {
-      if (selectedTokenIndex === 0) {
-        const tx = await launchContract.deposit(
-          {
-            token: tokenData.address,
-            amount: amount,
-          },
-          [],
-          { value: amount }
-        )
-        setTxHash(tx.hash)
-      } else {
-        await erc20.approve(
-          selectedChain.launchData.launchContractAddress,
-          amount
-        )
-        const tx = await launchContract.deposit(
-          {
-            token: tokenData.address,
-            amount: amount,
-          },
-          [],
-          {}
-        )
-        setTxHash(tx.hash)
-      }
-    } else {
-      if (selectedTokenIndex === 0) {
-        const tx = await launchContract.withdraw(
-          { token: tokenData.address, amount: amount },
-          true
-        )
-        setTxHash(tx.hash)
-      } else {
-        const tx = await launchContract.withdraw(
-          { token: tokenData.address, amount: amount },
-          false
-        )
-        setTxHash(tx.hash)
-      }
-    }
-  }
-
-  return [commitAssets, txHash, setTxHash]
-}
