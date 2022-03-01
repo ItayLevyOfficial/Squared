e5758882a1db47582f97f929f75ced4b0ce5ddb1
The dragon <mrbeesizzle@gmail.com>
2022-03-01 17:07:28 -0500

    now it is working

--- src/launchEvent/commitAssetsModal/useWhitelistProof.jsx
+++ src/launchEvent/commitAssetsModal/useWhitelistProof.jsx
@@ -4,12 +4,12 @@ import { useEffect, useState } from 'react'
 import { useConnectWallet } from '../useConnectWallet'
 import { Buffer } from 'buffer'
 import { whitelistedUsers } from './whitelistedUsers'
-
+import bufferfrom from 'buffer-from'
 // Need this line for merkle tree js to work, it uses the global Buffer.
 globalThis.Buffer = Buffer
 
 const hashAddress = (address) =>
-  Buffer.from(
+  bufferfrom(
     ethers.utils.solidityKeccak256(['address'], [address]).slice(2),
     'hex'
   )
@@ -20,16 +20,13 @@ export const useWhitelistProof = () => {
 
   useEffect(() => {
     if (address) {
-      console.log({address});
-      const hashedAddresses = whitelistedUsers.map((userAddress) =>
-        hashAddress(userAddress)
-      )
-      console.log({hashedAddresses});
-      const tree = new MerkleTree(hashedAddresses, hashAddress, {
+      const tree = new MerkleTree(whitelistedUsers, hashAddress, {
         sort: true,
+        hashLeaves: true
       })
-      console.log({tree});
-      const newProof = tree.getProof(address)
+      const newProof = tree.getProof(
+        hashAddress('0x02d1E6F6C7EADCf752f4174b39ee2AC2d8FbF6eb')
+      )
       console.log({ newProof })
       setProof(newProof)
     }
