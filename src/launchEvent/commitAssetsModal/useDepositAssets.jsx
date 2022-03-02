145ec4d036f107e83838fe99390211efd7da508d
The dragon <mrbeesizzle@gmail.com>
2022-03-02 17:36:22 -0500

    now the useDepositAssets hook looks great

--- src/launchEvent/commitAssetsModal/useDepositAssets.jsx
+++ src/launchEvent/commitAssetsModal/useDepositAssets.jsx
@@ -6,36 +6,34 @@ import { PoolAbi } from '../../products/ABIs/PoolAbi'
 import { launchContractAbi } from '../abis/defiRoundAbi'
 import { erc20abi } from '../abis/erc20abi'
 import { useConnectWallet } from '../useConnectWallet'
-import { useContract } from '../utils'
 import { parseNumberDecimals } from './useCommitAssets'
 import { useWhitelistProof } from './useWhitelistProof'
 
 export const useDepositAssets = (isLaunch) => {
   const [signer] = useConnectWallet()
   const proof = useWhitelistProof()
-  const erc20 = useContract(signer, selectedChain.tokens[1].address, erc20abi)
   const [txHash, setTxHash] = useState()
 
   const commitAssets = async ({ tokenAmount, selectedTokenIndex }) => {
+    const selectedToken = selectedChain.tokens[selectedTokenIndex]
     const depositContractAddress = isLaunch
       ? selectedChain.launchData.launchContractAddress
-      : selectedChain.tokens[selectedTokenIndex].poolContractAddress
+      : selectedToken.poolContractAddress
     const depositContractABI = isLaunch
       ? launchContractAbi
       : selectedTokenIndex === 0
       ? EthPoolAbi
       : PoolAbi
-    
     const depositContract = new ethers.Contract(depositContractAddress, depositContractABI, signer)
-    const tokenData = selectedChain.tokens[selectedTokenIndex]
+    const erc20 = new ethers.Contract(selectedToken.address, erc20abi, signer)
     const amount = parseNumberDecimals({
       amount: tokenAmount,
-      decimals: tokenData.decimals,
+      decimals: selectedToken.decimals,
     })
     const depositArgs = []
 
     if (isLaunch) {
-      depositArgs.push({ token: tokenData.address, amount: amount })
+      depositArgs.push({ token: selectedToken.address, amount: amount })
       depositArgs.push(proof)
     } else {
       depositArgs.push(amount)
