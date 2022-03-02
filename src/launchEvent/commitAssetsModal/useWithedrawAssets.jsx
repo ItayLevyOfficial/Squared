59719e58f592c99bf4830fe1b1824013feb481a7
The dragon <mrbeesizzle@gmail.com>
2022-03-02 14:33:24 -0500

    keep working

--- src/launchEvent/commitAssetsModal/useWithedrawAssets.jsx
+++ src/launchEvent/commitAssetsModal/useWithedrawAssets.jsx
@@ -1,12 +1,9 @@
-import { useContext, useState } from 'react'
+import { useState } from 'react'
 import { selectedChain } from '../../chains'
 import { launchContractAbi } from '../abis/defiRoundAbi'
-import { erc20abi } from '../abis/erc20abi'
-import { StageContext } from '../LaunchEventScreen'
 import { useConnectWallet } from '../useConnectWallet'
 import { useContract } from '../utils'
 import { parseNumberDecimals } from './useCommitAssets'
-import { useWhitelistProof } from './useWhitelistProof'
 
 export const useWithdrawAssets = () => {
   const [signer] = useConnectWallet()
@@ -31,69 +28,4 @@ export const useWithdrawAssets = () => {
   }
 
   return [withdrawAssets, txHash, setTxHash]
-}
-
-export const useCommitLaunchAssets = () => {
-  const [signer] = useConnectWallet()
-  const proof = useWhitelistProof()
-  const erc20 = useContract(signer, selectedChain.tokens[1].address, erc20abi)
-  const [txHash, setTxHash] = useState()
-  const launchContract = useContract(
-    signer,
-    selectedChain.launchData.launchContractAddress,
-    launchContractAbi
-  )
-  const launchStage = useContext(StageContext)
-
-  const commitAssets = async ({ tokenAmount, selectedTokenIndex }) => {
-    const tokenData = selectedChain.tokens[selectedTokenIndex]
-    const amount = parseNumberDecimals({
-      amount: tokenAmount,
-      decimals: tokenData.decimals,
-    })
-    if (launchStage === 1) {
-      if (selectedTokenIndex === 0) {
-        console.log({ proof })
-        const tx = await launchContract.deposit(
-          {
-            token: tokenData.address,
-            amount: amount,
-          },
-          proof,
-          { value: amount }
-        )
-        setTxHash(tx.hash)
-      } else {
-        await erc20.approve(
-          selectedChain.launchData.launchContractAddress,
-          amount
-        )
-        const tx = await launchContract.deposit(
-          {
-            token: tokenData.address,
-            amount: amount,
-          },
-          proof,
-          {}
-        )
-        setTxHash(tx.hash)
-      }
-    } else {
-      if (selectedTokenIndex === 0) {
-        const tx = await launchContract.withdraw(
-          { token: tokenData.address, amount: amount },
-          true
-        )
-        setTxHash(tx.hash)
-      } else {
-        const tx = await launchContract.withdraw(
-          { token: tokenData.address, amount: amount },
-          false
-        )
-        setTxHash(tx.hash)
-      }
-    }
-  }
-
-  return [commitAssets, txHash, setTxHash]
 }
