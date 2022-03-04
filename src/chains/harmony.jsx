b81a0ed872c891a8565aa7e498f73cea8d614ccb
The dragon <mrbeesizzle@gmail.com>
2022-03-04 17:57:35 -0500

    Now the tokens names are the harmony token names

--- src/chains/harmony.jsx
+++ src/chains/harmony.jsx
@@ -1,1 +1,15 @@
-export const 
+import { hardhat } from './hardhat'
+
+export const harmony = {
+  ...hardhat,
+  tokens: [
+    {
+      ...hardhat.tokens[0],
+      name: 'ONE',
+    },
+    {
+      ...hardhat.tokens[1],
+      name: 'oneUSD',
+    },
+  ],
+}
