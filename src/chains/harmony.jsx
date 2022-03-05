a090d673a638e0bb1d1a782c9e29f94eecd63349
The dragon <mrbeesizzle@gmail.com>
2022-03-05 18:20:57 -0500

    changed the one addresses to eth addresses

--- src/chains/harmony.jsx
+++ src/chains/harmony.jsx
@@ -8,20 +8,20 @@ export const harmony = {
     // TODO - before launch, update the the actual launch time.
     launchTime: Date.now() - 60 * 1000 * 60 * 12,
     lastLookStart: Date.now() - 60 * 1000 * 60 * 12,
-    launchContractAddress: 'one17ljvza5zh5yjph38dlfde2am0a8r0j4kmct5gq',
+    launchContractAddress: '0xf7e4c17682bd0920de276fd2dcabbb7f4e37cab6',
     launchTokensAmount: 3_000_000,
   },
   tokens: [
     {
       name: 'ONE',
       icon: <img src={HarmonyIcon} width={30} />,
-      address: 'one1eanyppa9hvpr0g966e6zs5hvdjxkngn6jtulua',
+      address: '0xcf664087a5bb0237a0bad6742852ec6c8d69a27a',
       decimals: 18,
     },
     {
       name: 'USDC',
       icon: <Usdc />,
-      address: 'one1np293efrmv74xyjcz0kk3sn53x0fm745f2hsuc',
+      address: '0x985458e523db3d53125813ed68c274899e9dfab4',
       decimals: 6,
     },
   ],
