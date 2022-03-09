588211c0a807afd7a17fdbcc66b48d3fa2076d83
The dragon <mrbeesizzle@gmail.com>
2022-03-08 19:25:28 -0500

    Now it works great

--- src/launchEvent/LaunchEventScreen.jsx
+++ src/launchEvent/LaunchEventScreen.jsx
@@ -1,5 +1,6 @@
 import { createContext, useCallback, useEffect, useState } from 'react'
 import { selectedChain } from '../constants'
+import { HomeScreen } from '../home/HomeScreen'
 import { launchContractAbi } from './abis/defiRoundAbi'
 import { Footer } from './Footer'
 import { LaunchScreenBody } from './LaunchScreenBody'
@@ -31,20 +32,29 @@ export const LaunchScreenContext = () => {
   }, [readLaunchContract])
   return (
     <StageContext.Provider value={stage + 1}>
-      <div className="fixed bottom-10 left-10 flex flex-col space-y-4">
-        <RoutingButton currentStage={stage} buttonStage={0} setStage={setStage}>Launch Event</RoutingButton>
-        <RoutingButton currentStage={stage} buttonStage={1} setStage={setStage}>Last Look</RoutingButton>
-        <RoutingButton currentStage={stage} buttonStage={2} setStage={setStage}>Cycle Zero</RoutingButton>
+      <div className="fixed bottom-10 left-10 flex flex-col space-y-3">
+        <RoutingButton currentStage={stage} buttonStage={0} setStage={setStage}>
+          Launch Event
+        </RoutingButton>
+        <RoutingButton currentStage={stage} buttonStage={1} setStage={setStage}>
+          Last Look
+        </RoutingButton>
+        <RoutingButton currentStage={stage} buttonStage={2} setStage={setStage}>
+          Cycle Zero
+        </RoutingButton>
       </div>
-      {[0, 1].includes(stage) ? <LaunchEventScreen /> : null}
+      {[0, 1].includes(stage) ? <LaunchEventScreen /> : <HomeScreen />}
     </StageContext.Provider>
   )
 }
 
-const RoutingButton = ({ children, currentStage, buttonStage, setStage}) => (
-  <button onClick={() => setStage(buttonStage)}
+const RoutingButton = ({ children, currentStage, buttonStage, setStage }) => (
+  <button
+    onClick={() => setStage(buttonStage)}
     className={`w-48 py-3 font-medium text-lg rounded-full text-center ${
-      currentStage === buttonStage ? 'bg-darkPrimary' : 'border-solid border-[0.5px] border-white'
+      currentStage === buttonStage
+        ? 'bg-darkPrimary'
+        : 'border-solid border-[0.5px] border-white'
     }`}
   >
     {children}
