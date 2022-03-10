a4aa4dc0c1ab6507a2c36e6b5e4ce5673a354eb0
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-03-10 12:16:12 -0500

    change_chain

--- src/constants.jsx
+++ src/constants.jsx
@@ -2,4 +2,4 @@ import { harmony } from './chains/harmony'
 import { hardhat } from './chains/hardhat'
 import { bsc } from './chains/bsc'
 
-export const selectedChain = hardhat
+export const selectedChain = harmony
