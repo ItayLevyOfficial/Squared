d5ca700dc1c572c09013da0f08a99b9c885911a2
The dragon <mrbeesizzle@gmail.com>
2022-03-10 14:33:07 -0500

    now it might work

--- src/Home/HomeStakingPools.jsx
+++ src/Home/HomeStakingPools.jsx
@@ -4,7 +4,7 @@ import { usePoolBalances } from './useErc20Functions'
 
 export const StakingPools = ({ className, openModal }) => {
   const listofPoolBalances = usePoolBalances()
-
+  console.log({ listofPoolBalances })
   return (
     <div
       className={`${className} h-full flex flex-col items-center justify-evenly`}
