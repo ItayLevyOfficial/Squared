49c1c83f11d60446a70fceab5c555e7958eb333c
The dragon <mrbeesizzle@gmail.com>
2022-03-12 18:13:50 -0500

    Now the kovan object first fields looks great

--- src/chains/kovan.jsx
+++ src/chains/kovan.jsx
@@ -1,14 +1,14 @@
 export const kovan = {
   chainId: '0x2A',
-  chainName: 'hardhat',
+  chainName: 'kovan',
   approvalTime: 2,
-  rpcUrls: ['http://127.0.0.1:8545/'],
+  rpcUrls: ['https://kovan.infura.io/v3/fd7b0a37ee4249039dbf93ac592eb34a'],
   launchData: {
     // TODO - before launch, update the the actual launch time.
     launchTime: Date.now() - 60 * 1000 * 60 * 12,
     lastLookStart: Date.now() - 60 * 1000 * 60 * 12,
-    launchContractAddress: '0xF32D39ff9f6Aa7a7A64d7a4F00a54826Ef791a55',
+    launchContractAddress: '0x1ac9a3D7ae101398C483ef3f44ddA5797024310f',
     launchTokensAmount: 3_000_000,
   },
-  scan: { name: 'Etherscan', url: 'https://etherscan.io/' },
+  scan: { name: 'Etherscan', url: 'https://kovan.etherscan.io/' },
 }
