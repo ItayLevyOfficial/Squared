7fd07090dd577a9eb363ae9823351a760a6a2ee4
The dragon <mrbeesizzle@gmail.com>
2022-03-24 21:24:13 -0500

    changed the chain to harmony

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,6 +1,6 @@
-import { kovan } from './chains/kovan'
+import { harmony } from './chains/harmony'
 
-export const selectedChain = kovan
+export const selectedChain = harmony
 
 export const launchEventArticle =
   'https://medium.com/@squared_finance/squared-the-take-off-61ac26589f32'
