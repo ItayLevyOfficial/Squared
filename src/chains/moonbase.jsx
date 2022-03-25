81c53ab8218f8a710bafef75676db05d04b8f73f
The dragon <mrbeesizzle@gmail.com>
2022-03-24 20:34:21 -0500

    updated the dev token values

--- src/chains/moonbase.jsx
+++ src/chains/moonbase.jsx
@@ -15,7 +15,7 @@ export const moonbase = {
   },
   tokens: [{
     name: 'DEV',
-    poolContractAddress: '0xB0f05d25e41FbC2b52013099ED9616f1206Ae21B',
+    poolContractAddress: '0x8A35F422B4D343b562799b95b96f4d31fE81b15b',
     address: '0x1436aE0dF0A8663F18c0Ec51d7e2E46591730715',
     decimals: '18',
     icon: <img src={DEVIcon} width={30} />,
