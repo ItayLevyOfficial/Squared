45f0d1d8480e66e67180647dd8b23ae893ed4e67
The dragon <mrbeesizzle@gmail.com>
2022-03-24 20:38:04 -0500

    updated the launch contract address

--- src/chains/moonbase.jsx
+++ src/chains/moonbase.jsx
@@ -4,7 +4,6 @@ import DEVIcon from './glmrIcon.png'
 import { Usdc } from '../launchEvent/icons/usdc'
 
 export const moonbase = {
-  ...hardhat,
   chainId: '0x507',
   chainName: 'moonbase',
   approvalTime: 2,
@@ -13,29 +12,39 @@ export const moonbase = {
     name: 'Moonbase Explorer',
     url: 'https://moonbase-blockscout.testnet.moonbeam.network',
   },
-  tokens: [{
-    name: 'DEV',
-    poolContractAddress: '0x8A35F422B4D343b562799b95b96f4d31fE81b15b',
-    address: '0x1436aE0dF0A8663F18c0Ec51d7e2E46591730715',
-    decimals: '18',
-    icon: <img src={DEVIcon} width={30} />,
-  },{
-    name: 'USDC',
-    poolContractAddress: '0x4caeaB5547d1FD5A6685E7A52e4a900d779B767f',
-    address: '0x0a863a4A66fBf86fb4D1F737945C836F571C403F',
-    decimals: '6',
-    icon: <Usdc />,
+  launchData: {
+    // TODO: before launch, update the the actual launch time.
+    launchTime: Date.now() - 60 * 1000 * 60 * 12,
+    lastLookStart: Date.now() - 60 * 1000 * 60 * 12,
+    launchContractAddress: '0x1ac9a3D7ae101398C483ef3f44ddA5797024310f',
+    launchTokensAmount: 3_000_000,
   },
-  {
-    name: 'SQRD',
-    poolContractAddress: '0x3e771689A5Bcc7d56cedbb0a202c9fe0E68B93f4',
-    address: '0x4314014f95A726B82A42cBCC6D9FAeC2851b54e8',
-    decimals: '6',
-  },
-  {
-    name: 'SQRD LP',
-    poolContractAddress: '0xD42912755319665397FF090fBB63B1a31aE87Cee',
-    address: '0x8A93d247134d91e0de6f96547cB0204e5BE8e5D8',
-    decimals: '6',
-  }]
+  tokens: [
+    {
+      name: 'DEV',
+      poolContractAddress: '0x8A35F422B4D343b562799b95b96f4d31fE81b15b',
+      address: '0x1436aE0dF0A8663F18c0Ec51d7e2E46591730715',
+      decimals: '18',
+      icon: <img src={DEVIcon} width={30} />,
+    },
+    {
+      name: 'USDC',
+      poolContractAddress: '0x4caeaB5547d1FD5A6685E7A52e4a900d779B767f',
+      address: '0x0a863a4A66fBf86fb4D1F737945C836F571C403F',
+      decimals: '6',
+      icon: <Usdc />,
+    },
+    {
+      name: 'SQRD',
+      poolContractAddress: '0x3e771689A5Bcc7d56cedbb0a202c9fe0E68B93f4',
+      address: '0x4314014f95A726B82A42cBCC6D9FAeC2851b54e8',
+      decimals: '6',
+    },
+    {
+      name: 'SQRD LP',
+      poolContractAddress: '0x22cC13e2b3740B0F23E37891Bd551572A25b9D6C',
+      address: '0x48B51bC25Aa41C1c6CeBc1797BF4FF5411354E5E',
+      decimals: '6',
+    },
+  ],
 }
