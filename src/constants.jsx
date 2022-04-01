9fb68804ecad25242ad7ec9b428596b788b9d7cc
Barry Kuts <72349261+barrykuts@users.noreply.github.com>
2022-04-01 15:25:21 -0400

    change_netowrk

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,8 +1,7 @@
 import { harmony } from './chains/harmony'
 import { kovan } from './chains/kovan'
-import { hardhat } from './chains/hardhat'
 
-export const selectedChain = hardhat
+export const selectedChain = kovan
 
 export const launchEventArticle =
   'https://medium.com/@squared_finance/squared-the-take-off-61ac26589f32'
